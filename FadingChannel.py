Added Snr graph generating script. FIxed math errors with snr, modulation normalization and disabled GUI picture control

FadingChannel.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plot
 class FadingChannel():
     def __init__(self, noiseDeviation = np.sqrt(0.5)):
         random.seed()
-        self.h = random.gauss(0, np.sqrt(0.5)) + 1j*random.gauss(0,np.sqrt(0.5))
+        self.h = random.gauss(0, 1/np.sqrt(2)) + 1j*random.gauss(0,1/np.sqrt(2))
         self.noise = random.gauss(0, noiseDeviation) + 1j*random.gauss(0, noiseDeviation)
        
     def ApplyFadingToTransmission(self, transmission):

GUI.py
@@ -153,7 +153,7 @@ class GUI(tk.Tk):
             pilotType = self.enumDictionary.get(self.channelEstimationMethod.get())
             decoderType = self.enumDictionary.get(self.decodeMethod.get())
             SNR = self.SNR.get()
-            noiseStandardDeviation = np.sqrt(np.power(10.0, (-1*SNR)))
+            noiseStandardDeviation = np.sqrt(np.power(10.0, (-1*SNR/10.0)))
             numReceivers = self.RecNum.get()
         except:
             print "Alert: Input Error"